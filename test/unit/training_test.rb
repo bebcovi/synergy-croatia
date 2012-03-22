ffaf4aa3f99425fb0bcb5b292b0959bffae22b04
Janko Marohnić <janko.marohnic@gmail.com>
2012-03-22 23:14:28 +0100

    Fix tests again

--- test/unit/training_test.rb
+++ test/unit/training_test.rb
@@ -2,20 +2,21 @@ require 'test_helper'
 
 class TrainingTest < ActiveSupport::TestCase
   test "attributes" do
-    model_attributes = Training.attribute_names
-    assert [:title, :level, :description, :goal, :beings_on, :ends_on,
-            :participating_countries, :age_restriction, :location,
-            :infoletter_url, :participation_form_url, :additional_info,
-            :created_at, :updated_at, :id].all? do |attribute|
-              attribute.in?(model_attributes)
-            end
+    model_attributes = Training.attribute_names.map(&:to_sym)
+    [:title, :level, :description, :goal, :begins_on, :ends_on,
+     :participating_countries, :age_restriction, :location,
+     :infoletter_url, :participation_form_url, :additional_info,
+     :created_at, :updated_at, :id].each do |attribute|
+       assert_includes(model_attributes, attribute)
+     end
   end
 
   test "methods" do
     require 'date'
     assert_equal 10, Training.new(:begins_on => Date.parse("1st Jan 2012"),
                                   :ends_on => Date.parse("10th Jan 2012")).duration
-    assert_instance_of Array, trainings(:ok).participating_countries
+    assert_instance_of Array, trainings(:ok).participating_countries,
+      "Training#participating_countries doesn't return an array"
     assert_equal %w[Croatia Romania France],
       Training.new(:participating_countries => "Croatia, Romania,France").participating_countries
   end
