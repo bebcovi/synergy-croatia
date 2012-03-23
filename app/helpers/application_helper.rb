0102b725c8aca3e59c885e14693782891686881b
Janko Marohnić <janko.marohnic@gmail.com>
2012-03-23 22:38:14 +0100

    #pages = #navigation_pages in ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   Page = Struct.new(:title, :route)
-  def pages
-    navigation_pages = [["index", root_path], ["about", about_us_path],
-                        ["projects", projects_path], ["collaboration", collaboration_path],
-                        ["experience", experience_path], ["method", method_path]]
+  def navigation_pages
+    pages = [["index", root_path], ["about", about_path],
+             ["projects", projects_path], ["collaboration", collaboration_path],
+             ["experience", experience_path], ["method", method_path]]
 
-    navigation_pages.collect do |page, route|
+    pages.collect do |page, route|
       Page.new(t("pages.#{page}.page_title"), route)
     end
   end
