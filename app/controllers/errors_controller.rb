fecc23bb7f23a9b800a96e8e86400a207fc168c0
Janko Marohnić <janko.marohnic@gmail.com>
2012-03-23 21:09:56 +0100

    Make translations more elegant

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,9 @@
 class ErrorsController < ApplicationController
   def not_found
-    render "404", :status => 404
+    render :not_found, :status => 404
   end
 
   def internal_server_error
-    render "500", :status => 500
+    render :internal_server_error, :status => 500
   end
 end
