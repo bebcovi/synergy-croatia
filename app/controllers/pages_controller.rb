aa3e57d38efffa3e57460bf9c07cd83f3d46777d
Janko Marohnić <janko.marohnic@gmail.com>
2012-03-25 23:58:15 +0200

    Fix an error when fetching future trainings

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ require 'date'
 
 class PagesController < ApplicationController
   def index
-    @trainings = Training.where("ends_on >= #{Date.today}")
+    @trainings = Training.where("ends_on >= '#{Date.today}'")
   end
 
   def about
