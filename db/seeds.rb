69d00672f4f61af33af69afbb37a46774d1d40e1
Matija Marohnić <matija.marohnic@gmail.com>
2012-04-20 01:44:17 +0200

    Fill testing seed with real content

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,14 @@ require 'date'
 
 Training.delete_all
 Training.create \
-  :title => "Novi trening",
-  :description => "Opis treninga...",
-  :goal => "Poševiti se",
-  :begins_on => Date.parse("3rd Jan 2015"),
-  :ends_on => Date.parse("10th Jan 2015"),
-  :participating_countries => "Hrvatska, Rumunjska i Mađarska",
-  :age_restriction => 18,
-  :location => "Bukurešt",
+  :title => "Practicing Coaching training",
+  :description => "Practicing Coaching je trening za one koji rade s pojedincima ili grupama, koji vole raditi na osnaživanju drugih, i usmjereni su poticanju najboljeg u svakoj osobi.\n\nOvaj sedmo-dnevni treningu u obliku coaching radionica daje priliku da polaznik svjesno koristi različite coaching tehnike usklađene sa vlastitim stilom. Vještine na treningu se stječu kroz iskustveno učenje. Treneri su Stella Saratsi and Marco Vlaming.",
+  :goal => nil,
+  :begins_on => Date.parse("4th May 2012"),
+  :ends_on => Date.parse("10th May 2012"),
+  :participating_countries => nil,
+  :age_restriction => nil,
+  :location => "Ommen, Nizozemska",
   :infoletter_url => "#",
   :participation_form_url => "#",
-  :additional_info => "Dodatne informacije..."
+  :additional_info => nil
