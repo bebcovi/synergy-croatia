9e89f8b85d6a34709be2a0e2aa53ab115aee1bfe
Janko Marohnić <janko.marohnic@gmail.com>
2012-06-25 02:12:58 +0200

    Fix uploading to Dropbox once and for all

--- app/models/training.rb
+++ app/models/training.rb
@@ -8,9 +8,24 @@ class Training < ActiveRecord::Base
 
   scope :upcoming, where("ends_on >= '#{Date.today}'")
 
-  before_update  :delete_files, :upload_files
-  before_create  :upload_files
-  before_destroy :delete_files
+  before_create do
+    self.infoletter = upload_file(infoletter_file) if infoletter_file.present?
+    self.participation_form = upload_file(participation_form_file) if participation_form_file.present?
+  end
+  before_update do
+    if infoletter_file.present?
+      delete_file(infoletter)
+      self.infoletter = upload_file(infoletter_file)
+    end
+    if participation_form_file.present?
+      delete_file(participation_form)
+      self.participation_form = upload_file(participation_form_file)
+    end
+  end
+  before_destroy do
+    delete_file(infoletter)
+    delete_file(participation_form)
+  end
 
   translates :title, :description, :infoletter, :participation_form,
     :additional_info, :summary
@@ -33,22 +48,13 @@ class Training < ActiveRecord::Base
 
   private
 
-  def upload_files
-    if infoletter_file.present?
-      response = dropbox_client.put_file(infoletter_file.original_filename, infoletter_file.read)
-      self.infoletter = File.basename(response['path'])
-    end
-    if participation_form_file.present?
-      response = dropbox_client.put_file(participation_form_file.original_filename, participation_form_file.read)
-      self.participation_form = File.basename(response['path'])
-    end
-    true
+  def upload_file(file)
+    response = dropbox_client.put_file(file.original_filename, file.read)
+    File.basename(response['path'])
   end
 
-  def delete_files
-    dropbox_client.file_delete(infoletter) rescue nil
-    dropbox_client.file_delete(participation_form) rescue nil
-    true
+  def delete_file(filename)
+    dropbox_client.file_delete(filename) rescue nil
   end
 
   def dropbox_client
