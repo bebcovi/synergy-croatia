823fbc678735327683282b38fbbad9de058bf7fc
Janko Marohnić <janko.marohnic@gmail.com>
2012-07-11 14:47:31 +0200

    Silence Haml and Postgres

--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,20 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+def silence(&block)
+  $stderr = StringIO.new
+  yield
+  $stderr = STDOUT
+end
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development staging test)))
+  silence { Bundler.require(*Rails.groups(:assets => %w(development staging test))) }
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
+
+undef silence
 
 module SynergyCroatia
   class Application < Rails::Application
