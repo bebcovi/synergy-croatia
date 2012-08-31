229c29cfc71aa2a7f3fb9b2a24702209b402d4ef
Janko Marohnić <janko.marohnic@gmail.com>
2012-08-31 13:17:37 +0200

    Revert some things in migrations

--- db/migrate/20120321223937_create_trainings.rb
+++ db/migrate/20120321223937_create_trainings.rb
@@ -1,26 +1,22 @@
 class CreateTrainings < ActiveRecord::Migration
   def change
     create_table :trainings do |t|
-      t.string :name
+      t.string :title_hr
+      t.string :title_en
       t.text :description_hr
       t.text :description_en
       t.text :summary_hr
       t.text :summary_en
-      t.text :expenses_hr
-      t.text :expenses_en
       t.date :begins_on
       t.date :ends_on
       t.string :participating_countries
       t.string :city_hr
       t.string :city_en
       t.string :country
-      t.integer :maximum_age
-      t.integer :minimum_age
       t.string :infoletter
       t.string :application_form
-      t.date :deadline
-      t.integer :available_space
-      t.references :category
+      t.integer :maximum_age
+      t.integer :minimum_age
 
       t.timestamps
     end
