87d651467ccbb2adcf63bc0263ae9590971f8a76
Janko Marohnić <janko.marohnic@gmail.com>
2013-01-18 12:57:50 +0100

    Show uploaded files when editing projects

--- app/helpers/admin/projects_helper.rb
+++ app/helpers/admin/projects_helper.rb
@@ -1,2 +1,5 @@
 module Admin::ProjectsHelper
+  def uploaded_file(file, text)
+    ProjectPresenter.new(nil, self).uploaded_file(file, text)
+  end
 end
--- app/presenters/project_presenter.rb
+++ app/presenters/project_presenter.rb
@@ -27,32 +27,23 @@ class ProjectPresenter < BasePresenter
     markdown project.summary
   end
 
-  def infoletter(text, options = {})
-    if project.infoletter.present?
-      extension = project.infoletter.original_filename[/(?<=\.)\w{3,4}$/]
-      if extension == "pdf"
+  def uploaded_file(file, text, options = {})
+    if file.original_filename.present?
+      if File.extname(file.original_filename) == ".pdf"
         options.update(target: "_blank")
-        @template.pdf_file text, project.infoletter.url, options
+        @template.pdf_file text, file.url, options
       else
-        @template.word_file text, project.infoletter.url, options
+        @template.word_file text, file.url, options
       end
-    else
-      nil
     end
   end
 
+  def infoletter(text, options = {})
+    uploaded_file(project.infoletter, text, options)
+  end
+
   def application_form(text, options = {})
-    if project.application_form.present?
-      extension = project.application_form.original_filename[/(?<=\.)\w{3,4}$/]
-      if extension == "pdf"
-        options.update(target: "_blank")
-        @template.pdf_file text, project.application_form.url, options
-      else
-        @template.word_file text, project.application_form.url, options
-      end
-    else
-      nil
-    end
+    uploaded_file(project.application_form, text, options)
   end
 
   def edit_button(text, options = {})
