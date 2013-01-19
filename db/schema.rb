4acc814aaad4bfa68cf2293171b202165d11b338
Matija Marohnić <matija.marohnic@gmail.com>
2013-01-19 14:50:06 +0100

    Auto generated DB stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130119132024) do
+ActiveRecord::Schema.define(:version => 20130119131452) do
 
   create_table "announcements", :force => true do |t|
     t.string   "content_hr"
