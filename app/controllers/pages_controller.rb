5e95cab47496c917bd45cd1eb8f04eb96f30f02b
Janko Marohnić <janko.marohnic@gmail.com>
2013-02-12 00:13:50 +0100

    Make forecoming projects show only if available in current language

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,7 +15,7 @@ class PagesController < ApplicationController
   end
 
   def archive
-    @projects = Project.forecoming
+    @projects = Project.forecoming.available_in(I18n.locale)
   end
 
   def testimonials
