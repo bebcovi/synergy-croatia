089b27272f5e32b436da98cbe1d407e94bd0d314
Janko Marohnić <janko.marohnic@gmail.com>
2013-11-21 10:44:48 +0100

    Don't use a CDN

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,3 @@
 SynergyCroatia::Application.configure do
-  config.action_controller.asset_host = "//d2bv4miti81n5g.cloudfront.net"
+  # config.action_controller.asset_host = "//d2bv4miti81n5g.cloudfront.net"
 end
