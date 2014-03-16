e107529ae3fb8dda33013b0883ff2e1076f3756e
Janko Marohnić <janko.marohnic@gmail.com>
2014-03-16 21:56:43 +0100

    Don't display "Gallery" until there are some albums

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def navigation_pages
     pages = %w[news about evs projects testimonials partners gallery contact]
+    pages.delete("gallery") if Album.count == 0
     pages.collect do |name|
       {
         title: (I18n.t!("pages.#{name}.navigation_title") rescue t("pages.#{name}.page_title")),
